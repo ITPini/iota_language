22eee9e3a83ff6b39a4337b06d233458da8995ca
ITPini <marc6571us@gmail.com>
2023-04-13 17:44:38 +0200

    Implemented HTTPS Request abstract class

--- IoT/src/main/java/edu/aau/g404/protocol/https/Request.java
+++ IoT/src/main/java/edu/aau/g404/protocol/https/Request.java
@@ -1,2 +1,15 @@
-package edu.aau.g404.protocol.https;public class Request {
+package edu.aau.g404.protocol.https;
+
+public abstract class Request {
+    protected String url;
+    protected String applicationKey;
+
+    public Request(String url, String applicationKey) {
+        this.url = url;
+        this.applicationKey = applicationKey;
+    }
+
+    public Request() {
+
+    }
 }
