2293760ef87477e582e121944e6d479e3a90c81d
Profft <stefanlarsen@live.dk>
2023-04-14 14:58:33 +0200

    WIP scanner (NOT READY FOR USE!)

---
+++ IoT/src/main/java/edu/aau/g404/AST.java
@@ -0,0 +1,13 @@
+package edu.aau.g404;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AST {
+
+    Map tokenType;
+
+    public AST() {
+        tokenType = new HashMap<String, >();
+    }
+}
--- IoT/src/main/java/edu/aau/g404/LexiScanner.java
+++ IoT/src/main/java/edu/aau/g404/LexiScanner.java
@@ -3,8 +3,15 @@ package edu.aau.g404;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.lang.Character.isDigit;
+import static java.lang.Character.isLetter;
 
 public class LexiScanner {
+
+    private Map keyTable;
 
     private FileReader file;
 
@@ -16,6 +23,14 @@ public class LexiScanner {
         } catch (FileNotFoundException e) { //custom error handler should be implemented at some point
             e.printStackTrace();
         }
+        keyTable = new HashMap<String, String>();
+        keyTable.put("Light", "Type");
+        keyTable.put("Sensor", "Type");
+        keyTable.put("Begin", "Type");
+        keyTable.put("Trigger", "Type");
+        keyTable.put("Action", "Type");
+        keyTable.put("Light", "Type");
+        keyTable.put("Light", "Type");
     }
 
     public int getCharPointer() {
@@ -26,29 +41,63 @@ public class LexiScanner {
         this.charPointer = charPointer;
     }
 
+    public void Scanner() {
+        char currentChar;
+        while ((currentChar = this.readNextChar()) != '$') {
+            if (currentChar == ' ' || currentChar == '\n') {
+            } else if (!isLetter(currentChar) && currentChar != '-' && currentChar != '_') {
+                switch (currentChar) {
+                    case '\"':
+                        ;
+                        break;
+                    case '*':
+                        ;
+                        break;
+                    case '+':
+                        ;
+                        break;
+                    case '.':
+                        ;
+                        break;
+                    case ',':
+                        ;
+                        break;
+                    case ';':
+                        ;
+                        break;
+                    default:
+                }
 
-    public char readNextChar(){
+            } else {
+                String currentWord = "";
+                currentWord += currentChar;
+                while (isLetter(currentChar = this.readNextChar()) || isDigit(currentChar) || currentChar == '-' || currentChar == '_') {
+                }
+
+
+            }
+        }
+    }
+
+    public char readNextChar() {
         try {
             this.charPointer = file.read();
         } catch (IOException e) { //custom error handler should be implemented at some point
             e.printStackTrace();
         }
-        if (charPointer==-1){
+        if (charPointer == -1) {
             return '$';
         } else {
-            return (char)charPointer;
+            return (char) charPointer;
         }
     }
 
-    public void printFile(){
+    public void printFile() {
         char currentChar;
-        while ((currentChar = this.readNextChar()) != '$'){
+        while ((currentChar = this.readNextChar()) != '$') {
             System.out.print(currentChar);
         }
     }
-
-
-
 
 
 }
