dc5290f72b47ade164c10eedb44a07b0c09ac092
Pini <marc6571us@gmail.com>
2023-04-20 12:37:40 +0200

    Added support for color

--- IoT/src/main/java/edu/aau/g404/device/Light.java
+++ IoT/src/main/java/edu/aau/g404/device/Light.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Light {
     @JsonProperty("id")
@@ -18,6 +21,9 @@ public class Light {
     @JsonProperty("dimming")
     private Dimming dimming;
 
+    @JsonProperty("color")
+    private Color color;
+
     public Light() {
     }
 
@@ -25,32 +31,40 @@ public class Light {
         return identifier;
     }
 
-    public void setIdentifier(String identifier) {
-        this.identifier = identifier;
-    }
-
     public MetaData getMetaData() {
         return metaData;
     }
 
-    public void setMetaData(MetaData metaData) {
+    public Light setMetaData(MetaData metaData) {
         this.metaData = metaData;
+        return this;
     }
 
     public On getOnState() {
         return onState;
     }
 
-    public void setOnState(On onState) {
+    public Light setOnState(On onState) {
         this.onState = onState;
+        return this;
     }
 
     public Dimming getDimming() {
         return dimming;
     }
 
-    public void setDimming(Dimming dimming) {
+    public Light setDimming(Dimming dimming) {
         this.dimming = dimming;
+        return this;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Light setColor(Color color) {
+        this.color = color;
+        return this;
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -66,8 +80,9 @@ public class Light {
             return name;
         }
 
-        public void setName(String name) {
+        public Light.MetaData setName(String name) {
             this.name = name;
+            return this;
         }
     }
 
@@ -88,8 +103,9 @@ public class Light {
             return on;
         }
 
-        public void setOn(boolean on) {
+        public Light.On setOn(boolean on) {
             this.on = on;
+            return this;
         }
     }
 
@@ -110,8 +126,47 @@ public class Light {
             return brightness;
         }
 
-        public void setBrightness(float brightness) {
+        public Light.Dimming setBrightness(float brightness) {
             this.brightness = brightness;
+            return this;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Color {
+
+        @JsonCreator
+        public Color(@JsonProperty("xy") Map<String, Double> xy) {
+            this.xy = xy;
+        }
+
+        public Color(double x, double y){
+            this.xy.put("x", x);
+            this.xy.put("y", y);
+        }
+
+        public Color() {
+        }
+
+        @JsonProperty("xy")
+        private Map<String, Double> xy = new HashMap<>();
+
+        public double getX() {
+            return xy.get("x");
+        }
+
+        public Color setX(double x) {
+            this.xy.put("x", x);
+            return this;
+        }
+
+        public double getY() {
+            return xy.get("y");
+        }
+
+        public Color setY(double y) {
+            this.xy.put("y", y);
+            return this;
         }
     }
 }
