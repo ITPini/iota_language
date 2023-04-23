23e8190fdbcf1f05c029ca35f2db80a2e7efc371
ITPini <marc6571us@gmail.com>
2023-04-24 01:03:28 +0200

    WIP implementations of HTTPS POST request

--- IoT/src/main/java/edu/aau/g404/protocol/https/POST.java
+++ IoT/src/main/java/edu/aau/g404/protocol/https/POST.java
@@ -1,2 +1,14 @@
-package edu.aau.g404.protocol.https;public class POST {
+package edu.aau.g404.protocol.https;
+
+public class POST extends Request {
+    public POST(String url, String applicationKey) {
+        super(url, applicationKey);
+        super.requestType = "POST";
+    }
+
+    public POST() {
+        super.requestType = "POST";
+    }
+
+
 }
