61ecdb2896778c6c48558b2789808182c011549b
ITPini <marc6571us@gmail.com>
2023-04-24 01:07:25 +0200

    Added unit test for UDP request - WIP

---
+++ IoT/src/test/edu/aau/g404/protocol/udp/RequestTest.java
@@ -0,0 +1,31 @@
+package edu.aau.g404.protocol.udp;
+
+import edu.aau.g404.api.wiz.WiZLight;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class RequestTest {
+
+    private static final Request udp = new Request();
+
+    @BeforeEach
+    void setUp() {
+        udp.setIp("192.168.0.106").setPort(38899);
+    }
+
+    @Test
+    void request() {
+        WiZLight light = new WiZLight(1, "setState", new WiZLight.Params());
+        light.setBrightness(50);
+
+        udp.request(light);
+
+        udp.setIp("192.168.0.107");
+        udp.request(light);
+    }
+}
