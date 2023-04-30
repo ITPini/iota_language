968a74f7023077e29f507038956279bd196426e8
ITPini <marc6571us@gmail.com>
2023-05-01 00:35:22 +0200

    Redid new unit tests

--- IoT/src/test/edu/aau/g404/api/hue/HueLightTest.java
+++ IoT/src/test/edu/aau/g404/api/hue/HueLightTest.java
@@ -1,14 +1,17 @@
 package edu.aau.g404.api.hue;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 class HueLightTest {
-    private static HueLight testLight = new HueLight();
+    private HueLight testLight;
+
+    @BeforeEach
+    void init() {
+        testLight = new HueLight();
+    }
 
     @Test
     void rgbToXY() {
@@ -22,5 +25,43 @@ class HueLightTest {
         assertEquals(1, testLight.toLinearRGB(255));
         assertEquals(0.12743768043, testLight.toLinearRGB(100), 0.0001);
         assertEquals(0.00367650732, testLight.toLinearRGB(12), 0.0001);
+    }
+
+    @Test
+    void isOn() {
+        testLight.isOn(true);
+        assertTrue(testLight.getOnState().isOn());
+    }
+
+    @Test
+    void setBrightness() {
+        testLight.setBrightness(0.64f);
+        assertEquals(0.64f, testLight.getDimming().getBrightness());
+    }
+
+    @Test
+    void setColor() {
+        testLight.setColor(255, 100, 12);
+        double[] xy = testLight.rgbToXY(255, 100, 12);
+        assertEquals(xy[0], testLight.getColor().getX(), 0.0001);
+        assertEquals(xy[1], testLight.getColor().getY(), 0.0001);
+    }
+
+    @Test
+    void gettersAndSetters() {
+        HueLight.MetaData metaData = new HueLight.MetaData().setName("TestName");
+        HueLight.On onState = new HueLight.On(true);
+        HueLight.Dimming dimming = new HueLight.Dimming(0.75f);
+        HueLight.Color color = new HueLight.Color(0.64180971468, 0.34607322094);
+
+        testLight.setMetaData(metaData)
+                .setOnState(onState)
+                .setDimming(dimming)
+                .setColor(color);
+
+        assertEquals(metaData, testLight.getMetaData());
+        assertEquals(onState, testLight.getOnState());
+        assertEquals(dimming, testLight.getDimming());
+        assertEquals(color, testLight.getColor());
     }
 }
