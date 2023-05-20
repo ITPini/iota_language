5ab7642141c7c53661d5e129b44a2b26795ac2d5
ITPini <marc6571us@gmail.com>
2023-05-20 19:03:14 +0200

    Better readability

--- IoT/src/main/java/edu/aau/g404/protocol/https/SSLHelper.java
+++ IoT/src/main/java/edu/aau/g404/protocol/https/SSLHelper.java
@@ -12,15 +12,27 @@ public final class SSLHelper {
      * Disables SSL certificate verification for the application.
      * This method should only be used in development and testing environments.
      * Using this method in a production environment is a security risk.
+     * We create an array of TrustManager objects and populate it with a single anonymous implementation of X509TrustManager.
+     * This implementation overrides getAcceptedIssuers(), checkClientTrusted() and checkServerTrusted() to not perform any checks at all.
      * @throws Exception If an error occurs while disabling SSL certificate verification.
+     * @see <a href="https://gist.github.com/mhewedy/9cb0f3ab14c607abdd8a3e7c455be19e"></a>
      */
     public static void disableSSLVerification() throws Exception {
         // Create a trust manager that does not perform any certificate verification
         TrustManager[] trustAllCerts = new TrustManager[] {
                 new X509TrustManager() {
-                    public X509Certificate[] getAcceptedIssuers() { return null; }
-                    public void checkClientTrusted(X509Certificate[] certs, String authType) {}
-                    public void checkServerTrusted(X509Certificate[] certs, String authType) {}
+                    @Override
+                    public X509Certificate[] getAcceptedIssuers() {
+                        return null;
+                    }
+                    @Override
+                    public void checkClientTrusted(X509Certificate[] certs, String authType) {
+
+                    }
+                    @Override
+                    public void checkServerTrusted(X509Certificate[] certs, String authType) {
+
+                    }
                 }
         };
 
